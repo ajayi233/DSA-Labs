364b592193f909e174f73425638f9e2085513534
Lotsu Daniel Ajayi <daniel.lotsu@amalitech.com>
2024-11-16 17:22:36 +0000

    added the create user endpoint

--- node_lab_01/index.js
+++ node_lab_01/index.js
@@ -1,12 +1,12 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
+const querystring = require('querystring')
 
 let htmlpath = path.join(__dirname, "index.html");
 let userpath = path.join(__dirname, "users.html");
 const server = http.createServer((req, res) => {
   let pathName = req.url;
-
 
   // for the index page
   if (pathName === "/") {
@@ -32,11 +32,40 @@ const server = http.createServer((req, res) => {
       }
     })
   }
+//create-user endpoint to console log the username
+else if (pathName === '/create-user' && req.method === 'POST') {
+  let body = '';
 
-  else if(pathName === '/create-user'){
-    
-  }
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        
+        req.on('end', () => {
+            const parsedBody = querystring.parse(body);
+            const username = parsedBody.username;
+
+              //to handle any errors that may come up during username extraction
+            if (username) {
+                console.log('Username:', username);
+            } else {
+                console.log('Username not provided!');
+            }
+
+            res.writeHead(302, { Location: '/' }); // Redirect back to the homepage
+            res.end();
+  });
+}
+
+//to handle other routes which are not in our scope 
+else {
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Page not found');
+}
+
+
 });
+
+
 
 let port = 3000;
 server.listen(port, "localhost", () => {
