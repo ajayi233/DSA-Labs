c747f711f16c208e4631a17e9be5d5a1a22cddd7
Lotsu Daniel Ajayi <daniel.lotsu@amalitech.com>
2024-11-18 19:17:10 +0000

    text formatting

--- express_01/index.js
+++ express_01/index.js
@@ -1,44 +1,39 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
 //initializing app
 const app = express();
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-const indexPage = path.join(__dirname ,'public/index.html')
-const usersPage = path.join(__dirname, 'public/users.html')
+const indexPage = path.join(__dirname, "public/index.html");
+const usersPage = path.join(__dirname, "public/users.html");
 
 //routes
-app.get('/', (req,res) => {
-    res.sendFile(indexPage)
-})
+app.get("/", (req, res) => {
+  res.sendFile(indexPage);
+});
 
-app.get('/users', (req, res) =>{
-    res.sendFile(usersPage)
-})
+app.get("/users", (req, res) => {
+  res.sendFile(usersPage);
+});
 
-app.post('/create-user', (req, res) => {
-    let name = req.body
-    console.log(name.username);
-    res.redirect('/')
-})
-
+app.post("/create-user", (req, res) => {
+  let name = req.body;
+  console.log(name.username);
+  res.redirect("/");
+});
 
 //error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack) // Log the error stack trace
-    res.status(500).send('Something went wrong! Please try again later.') 
-})
-
-
+  console.error(err.stack); // Log the error stack trace
+  res.status(500).send("Something went wrong! Please try again later.");
+});
 
 //declaring port
-const PORT = process.env.PORT || 3000
-
+const PORT = process.env.PORT || 3000;
 
 //listen for request
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    
-})
+  console.log(`Server is running on port ${PORT}`);
+});
