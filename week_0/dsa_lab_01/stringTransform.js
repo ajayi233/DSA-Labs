aa3b23e818bfa27f9ace4970a8bafa68d17f28ca
Lotsu Daniel Ajayi <daniel.lotsu@amalitech.com>
2024-11-19 11:57:29 +0000

    restructuring made

---
+++ week_0/dsa_lab_01/stringTransform.js
@@ -0,0 +1,54 @@
+//first lab
+// function capitalize(text) {
+//   let newText = text.split("");
+//   if (text && typeof text === "string" && newText) {
+//     let capitalized = newText[0].toUpperCase();
+//     let newStr = newText.splice(1).join("");
+//     let final = capitalized + newStr;
+//     return final.trim();
+//   } else {
+//     return `Error!!!, no text found`;
+//   }
+// }
+// capitalize(5);
+
+// second lab
+// function reverse(text) {
+//   if (text && typeof text === "string") {
+//     return text.split("").reverse("").join("");
+//   } else {
+//     return `Error, no text found`;
+//   }
+// }
+// reverse("mountain");
+
+// //third lab
+function isPalindrome(str) {
+  if (str && typeof str === "string") {
+    let reversed = str.split("").reverse("").join("");
+    if (reversed === str) {
+      return true + ` ${str}, is a palindrome`;
+    }
+  } else {
+    return `Couldn't perform operation`;
+  }
+}
+isPalindrome("racecar");
+
+// //fourth lab
+// function wordCount(str) {
+//   if (typeof str !== "string") {
+//     return "could not be completed";
+//   }
+//   newString = str.replace(/\s/g, "");
+//   return newString.length;
+// }
+// // console.log(wordCount('Daniel is here'))
+
+// //composite function
+// const compositeFunction =
+//   (...fn) =>
+//   (string) =>
+//     fn.reduce((accumulator, currentValue) => currentValue(accumulator), string);
+// const result = compositeFunction(reverse, wordCount);
+// console.log(result("Daniel is here"));
